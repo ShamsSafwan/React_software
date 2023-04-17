3bf2e02c40098589cb9c6ea986eabf3d3c192a38
ShamsSafwan <41986366+ShamsSafwan@users.noreply.github.com>
2023-04-17 13:05:32 +0000

    Initials

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./App";
-
+import "./styles.css";
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
