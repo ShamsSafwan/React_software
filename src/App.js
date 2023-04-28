efa430bcbaf7cdb6e5abf28295fcd7e5d5ab5156
ShamsSafwan <41986366+ShamsSafwan@users.noreply.github.com>
2023-04-28 06:23:30 +0000

    3 tests passed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import "./styles.css";
+import { useState } from "react";
+function TextHolder  ({ onInputChange }){
+  return(
+    <textarea id="editor"  onChange={onInputChange}></textarea>
+  );
+}
 
+function Preview({ value }) {
+  return (
+    <div id ="preview" >{ value }</div>
+  );
+}
 export default function App() {
+
+  const [text, setText]=useState("");
+
+  function handleInputChange(e){
+    setText(e.target.value);
+  }
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <textarea id="editor"></textarea>
+      <h1>Markdown Previewer</h1>
+      <TextHolder onInputChange={handleInputChange}/>
+      <Preview value={text} />
+      
     </div>
   );
 }
