a6427aaf60b2431255bb5964a7040f1e08abd2f4
ShamsSafwan <41986366+ShamsSafwan@users.noreply.github.com>
2023-06-15 10:37:48 +0000

    Done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import "./styles.css";
 import { useState } from "react";
-function TextHolder  ({ onInputChange }){
-  return(
-    <textarea id="editor"  onChange={onInputChange}></textarea>
-  );
-}
+import { marked } from "marked";
 
-function Preview({ value }) {
-  return (
-    <div id ="preview" >{ value }</div>
-  );
-}
+
+
 export default function App() {
 
-  const [text, setText]=useState("");
+  const [text, setText]=useState(`
+  # H1 Welcome to my React Markdown Previewer!
+  ## H2 use formatting syntax to see the output on desired format. Use the cheatsheet from link below
+  [Click here for markdown cheatsheet](https://www.markdownguide.org/cheat-sheet/)
+  \`code\`
+  \`\`\`
+{
+  "firstName": "John",
+  "lastName": "Smith",
+  "age": 25
+}
+\`\`\`
+1. First item
+2. Second item
+  > you can delete all the text that is preloaded on editor and start writing yours
+  ![alt text](image.jpg)
+  **bold text**
+  `);
 
   function handleInputChange(e){
     setText(e.target.value);
@@ -21,8 +31,11 @@ export default function App() {
   return (
     <div className="App">
       <h1>Markdown Previewer</h1>
-      <TextHolder onInputChange={handleInputChange}/>
-      <Preview value={text} />
+      <textarea id="editor" onChange={handleInputChange} value={text}></textarea>
+      <div
+        id="preview"
+        dangerouslySetInnerHTML={{ __html: marked(text,{ breaks: true }) }}
+      ></div>
       
     </div>
   );
